8b078fd2b867da3c00ced155fc5150e4305ebe48
Rushikesh Sawant <sawantrushikesh82@gmail.com>
2025-11-25 20:50:50 +0530

    added server.go

---
+++ server-go/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, World!")
+}
+
+func main() {
+	http.HandleFunc("/hello", helloHandler)
+	log.Println("Starting server on :8080")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatalf("Could not start server: %s\n", err.Error())
+	}
+}
