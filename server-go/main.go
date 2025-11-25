b8dc81411dd1cac55c01b2a15ea8aa37c7f70f7d
Rushikesh Sawant <sawantrushikesh82@gmail.com>
2025-11-25 22:31:36 +0530

    added new module in server-go/main.js

--- server-go/main.go
+++ server-go/main.go
@@ -10,8 +10,19 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello, World!")
 }
 
+func Add(a int, b int) int {
+	return a + b
+}
+
 func main() {
 	http.HandleFunc("/hello", helloHandler)
+	http.HandleFunc("/add", func(w http.ResponseWriter, r *http.Request) {
+		a := 3
+		b := 5
+		result := Add(a, b)
+		fmt.Fprintf(w, "The sum of %d and %d is %d", a, b, result)
+	})
+
 	log.Println("Starting server on :8080")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatalf("Could not start server: %s\n", err.Error())
